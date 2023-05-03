07ea4efbb3075dbf231bb4b8085d1afebf3d5ee3
Cazazazoo <zhuconnie123@csu.fullerton.edu>
2023-05-03 12:47:51 -0700

    5/3/23

--- app/src/main/java/com/example/quizlingo/Question4Activity.kt
+++ app/src/main/java/com/example/quizlingo/Question4Activity.kt
@@ -29,5 +29,24 @@ class Question4Activity : AppCompatActivity() {
             val Intent = Intent(this, QuizResultsActivity::class.java)
             startActivity(Intent)
         }
+
+
+        val photoAPIFragment = this.supportFragmentManager.findFragmentById(R.id.photofragment_container)
+        if (photoAPIFragment == null) {
+            val fragment = PhotoAPIFragment()
+            this.supportFragmentManager
+                .beginTransaction()
+                .add(R.id.photofragment_container, fragment)
+                .commit()
+        }
+
+        val translatorAPIFragment = this.supportFragmentManager.findFragmentById(R.id.translatorfragment_container)
+        if (translatorAPIFragment == null) {
+            val fragment = TranslatorAPIFragment()
+            this.supportFragmentManager
+                .beginTransaction()
+                .add(R.id.translatorfragment_container, fragment)
+                .commit()
+        }
     }
 }
---
+++ app/src/main/java/com/example/quizlingo/TranslatorAPIFragment.kt
@@ -0,0 +1,14 @@
+package com.example.quizlingo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class TranslatorAPIFragment: Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+
+}
